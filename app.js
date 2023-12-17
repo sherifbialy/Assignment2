422d2b9d6a7ede0e8ae683bb791316aa7b62c04c
Sherif Bialy <sherifmoataz12@gmail.com>
2023-12-17 15:09:56 +0200

    On branch master Your branch is up to date with 'origin/master'.
    Fixes:
    	modified:   app.js
    	modified:   index.html
    	modified:   style.css

--- app.js
+++ app.js
@@ -13,16 +13,16 @@
     let input=x.innerText;
     console.log(input);
     let wordArr=input.split(" ");
-    //console.log(wordArr);
+    
 
-   // maxLength
-   wordArr=wordArr.filter(element => element.length>0);
    
+   wordArr=wordArr.filter(element => element.length>0);
+   console.log(wordArr);
   
-   if(wordArr.length!=0){
+   
     wordArr.forEach(element => {
         if(element!=" " || element!=""){
-            console.log(wordArr);
+            //console.log(wordArr);
            counter++; 
           if(element.split("").length>longestWordLength){
             longestWordLength=element.split("").length;
@@ -31,7 +31,7 @@
         }
     });
 
-   }   
+   
     
     
     let newText=""
@@ -39,9 +39,9 @@
         newText = input.replace(new RegExp(longestWord, "g"), `<span>${longestWord}</span>`);
     }
     
-    if(longestWord==wordArr[wordArr.length-1]){
-        newText+="&nbsp;";
-    }
+    // if(longestWord==wordArr[wordArr.length-1]){
+    //     newText+="&nbsp;";
+    // }
     x.innerHTML=newText;
     let y=document.getElementById('word-count');
     y.innerHTML=`Your word count is ${counter}`;
